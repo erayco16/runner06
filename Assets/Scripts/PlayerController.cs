e8403a3807591dfa4439f9f653c63a988208e15a
erayco16 <eray.san.pb@gmail.com>
2026-01-30 14:19:48 +0300

    runner06

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] Animator anim;
+    [SerializeField] float speed;
+    [SerializeField] float shift = 2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +15,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        /*if (Input.GetKey(KeyCode.A))
         {
             anim.SetBool("Run", true);
         }
@@ -28,6 +30,18 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.Space))
         {
             anim.SetBool("Jump", false);
+        }
+        */
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.x>-0.5f)
+        {
+            transform.Translate(new Vector3(-shift, 0, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && transform.position.x<0.5)
+        {
+            transform.Translate(new Vector3(shift, 0, 0));
         }
     }
 }
